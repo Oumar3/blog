ecbfcd0b4b4a5937e46ff8000ed2b6332f8afb06
Oumar3 <oumaralitori1@gmail.com>
2024-10-31 13:52:07 +0100

    nothing

--- authentification/urls.py
+++ authentification/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('login/',views.loginform,name='loginin'),
     path('logout/',views.logout_view,name='logout'),
     path('registerform/',views.registerform,name='registerform'),
-    
     path('register/',RegisterView.as_view(), name='register'),
 ]
